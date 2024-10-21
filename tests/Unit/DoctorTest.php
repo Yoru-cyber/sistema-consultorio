3a5fe253569f06b78f33eb261b33cd5791478819
Yoru-cyber <carlosmendez170210@gmail.com>
2024-10-20 21:59:20 -0400

    added fillable attributes to doctor model and created test

---
+++ tests/Unit/DoctorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Doctor;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DoctorTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_create_a_patient_with_fillable_attributes()
+    {
+        $data = [
+            'name' => 'John Doe',
+            'dni' => 123456789,
+        ];
+        $doctor = Doctor::create($data);
+        $this->assertModelExists($doctor);
+
+        $this->assertEquals('John Doe', $doctor->name);
+        $this->assertEquals(123456789, $doctor->dni);
+    }
+}
