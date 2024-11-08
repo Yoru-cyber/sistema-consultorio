6df3c99f4a6b27f5cc4f6a644ea84f87c352d5c2
Yoru-cyber <carlosmendez170210@gmail.com>
2024-11-08 02:36:49 -0400

    implemented functionality on recipe seeder and factory

--- database/seeders/RecipeSeeder.php
+++ database/seeders/RecipeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Recipe;
 
 class RecipeSeeder extends Seeder
 {
@@ -13,5 +14,6 @@ class RecipeSeeder extends Seeder
     public function run(): void
     {
         //
+        Recipe::factory()->count(20)->create();
     }
 }
