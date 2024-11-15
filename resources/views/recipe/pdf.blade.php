c7aefeae60b37c0c32493f000c0b8195f424d12f
Yoru-cyber <carlosmendez170210@gmail.com>
2024-11-15 05:02:36 -0400

    missing html tags

--- resources/views/recipe/pdf.blade.php
+++ resources/views/recipe/pdf.blade.php
@@ -1,12 +1,26 @@
-<p style="font-size: x-large">Clínica</p>
-<p>{{ $recipe->patient->name }}</p>
-<p>{{ $recipe->patient->dni }}</p>
-<p>Dr. {{ $recipe->doctor->name }}</p>
-<p>{{ $recipe->date }}</p>
-<p>{{ $recipe->prescription }}</p>
-<style>
-    * {
-        color: black;
-        text-align: center;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$recipe->patient->name}}</title>
+</head>
+
+<body>
+    <p style="font-size: x-large">Clínica</p>
+    <p>{{ $recipe->patient->name }}</p>
+    <p>{{ $recipe->patient->dni }}</p>
+    <p>Dr. {{ $recipe->doctor->name }}</p>
+    <p>{{ $recipe->date }}</p>
+    <p>{{ $recipe->prescription }}</p>
+    <style>
+        * {
+            color: black;
+            text-align: center;
+        }
+    </style>
+
+</body>
+
+</html>
