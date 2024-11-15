bdc15a5cebefa121461e232033ffc6a00399a72b
Yoru-cyber <carlosmendez170210@gmail.com>
2024-11-15 07:02:37 -0400

    added fillable attributes and relationships on certificate model

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Certificate extends Model
 {
     /** @use HasFactory<\Database\Factories\CertificateFactory> */
     use HasFactory;
+    protected $fillable = ['patient_id', 'doctor_id', 'date', 'description'];
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+    public function doctor(): BelongsTo
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
